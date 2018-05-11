203f124781056110039707a6ed78046840b4c823
Divan Visagie <visagiedivan@gmail.com>
2018-05-11 14:11:28 +0200

    define some stuff

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <string.h>
+#include <wchar.h>
+
+const char* fehu = "ᚠ";
+const char* uruz = "ᚢ";
+
+
+int get_unicode(char character) {
+    return (int) character;
+}
+
 
 int main(int argc, char ** argv) {
-    printf("Hello World \n");
-    
+
+    printf("%d \n", get_unicode('a'));
+    printf("%d \n", get_unicode('b'));
+    printf("%d \n", get_unicode('c'));
+
+    printf("%s \n", fehu);
+    printf("%s \n", uruz);
+
     return 0;
 }
