0d37114f4c74fffa6ad761653b7cc2b556a343dc
Divan Visagie <me@dvisagie.com>
2018-05-18 21:38:07 +0200

    optimise build

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,17 @@
 
 if git describe --exact-match --tags HEAD
 then
+    if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+      brew update;
+      brew install sbt;
+      brew tap tcnksm/ghr;
+      brew install ghr;
+    else
+      sudo apt-get install -y fakeroot;
+      curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme;
+      chmod +x ~/bin/gimme;
+      go get -u github.com/tcnksm/ghr; 
+    fi
     echo "Found tag"
       tag=$(git describe --exact-match --tags HEAD)
     echo "Found tag $tag"
